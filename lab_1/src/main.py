2aa4e23b4f0a69d23f8c76711a29aac7e89026ef
Marta Fedyshyn <martavfedyshyn@gmail.com>
2016-10-02 20:17:48 +0300

    Update main.py

--- lab_1/src/main.py
+++ lab_1/src/main.py
@@ -5,15 +5,6 @@ from user import User
 import os
 
 
-def read_from_file():
-    file = open('data.txt', 'r')
-    split_lines = []
-    for line in file:
-        split_lines.append(line.replace('\n', '').split(' ', -1))
-    file.close()
-    return split_lines
-
-
 def write_to_file(cur_line, new_line):
     fd, abs_path = mkstemp()
     with open(abs_path, 'w') as new_file:
@@ -25,7 +16,7 @@ def write_to_file(cur_line, new_line):
     move(abs_path, 'data.txt')  # Move new file
 
 
-def writing_new_password(login_name, password, split_lines, item, first_try):
+def writing_new_password(login_name, password, item, first_try):
     fd, abs_path = mkstemp()
     with open(abs_path, 'w') as new_file:
         with open('data.txt') as old_file:
@@ -37,14 +28,14 @@ def writing_new_password(login_name, password, split_lines, item, first_try):
                         new_file.write(line)
             else:
                 for line in old_file:
-                    new_file.write(line.replace(login_name + ' ' + str(split_lines[item][1]) + ' ' +
-                                                str(split_lines[item][2]) + ' ' +
-                                                str(split_lines[item][3]) + ' ' +
+                    new_file.write(line.replace(login_name + ' ' + str(person_list[item].is_blocked) + ' ' +
+                                                str(person_list[item].is_limit_on) + ' ' +
+                                                str(person_list[item].is_admin + ' ' +
                                                 str(password), login_name + ' ' +
-                                                str(split_lines[item][1]) + ' ' +
-                                                str(split_lines[item][2]) + ' ' +
-                                                str(split_lines[item][3]) + ' ' +
-                                                str(first_try)))
+                                                str(person_list[item].is_blocked) + ' ' +
+                                                str(person_list[item].is_limit_on) + ' ' +
+                                                str(person_list[item].is_admin) + ' ' +
+                                                str(first_try))))
     os.close(fd)
     os.remove('data.txt')  # Remove original file
     move(abs_path, 'data.txt')  # Move new file
@@ -69,23 +60,23 @@ def check_password(password):
     return flag1 and flag2
 
 
-def enter_new_password(login_name, password, split_lines, item):
+def enter_new_password(login_name, password, item):
     try:
         flag = False
         while not flag:
             first_try = raw_input('Enter new password: ')
-            if ast.literal_eval(split_lines[item][2]):
+            if ast.literal_eval(person_list[item].is_limit_on):
                 print 'flag ' + str(flag)
-                flag = check_password(login_name, split_lines, first_try)
+                flag = check_password(login_name, first_try)
                 if not flag:
                     print 'Look for password limits'
             else:
                 flag = True
         second_try = raw_input('Enter new password once more: ')
         if first_try == second_try:
-            writing_new_password(login_name, password, split_lines, item, first_try)
+            writing_new_password(login_name, password, item, first_try)
             print 'Password changed successfully\n'
-            if login_name == split_lines[0][0]:
+            if login_name == person_list[0].name:
                 show_admin_panel(login_name, first_try)
             else:
                 show_user_panel(login_name, first_try)
@@ -96,50 +87,44 @@ def enter_new_password(login_name, password, split_lines, item):
 
 
 def change_password(login_name, password):
-    split_lines = read_from_file()
     second_password = raw_input('Current password: ')
     if (second_password == password) or (second_password == password and password == ''):
-        for item in xrange(len(split_lines)):
-            if login_name in split_lines[item][0]:
-                if len(split_lines[item]) < 5:
-                    enter_new_password(login_name, '', split_lines, item)
+        for item in xrange(person_list.__len__()):
+            if login_name in person_list[item].name:
+                if person_list[item].password == '':
+                    enter_new_password(login_name, '', item)
                 # elif password != split_lines[item][4]:
                 #     print 'Password is incorrect, to change password enter correct one'
                 else:
-                    enter_new_password(login_name, password, split_lines, item)
+                    enter_new_password(login_name, password, item)
     else:
         print 'Password is incorrect, to change password enter correct one'
 
 
 def list_of_users():
-    split_lines = read_from_file()
     print 'user_name'.rjust(9), 'is_blocked'.rjust(9), 'limit_on'.rjust(9), \
         'is_admin'.rjust(9),  'password'.rjust(9)
-    for i in xrange(len(split_lines)):
+    for i in xrange(person_list.__len__()):
         try:
-            print str(split_lines[i][0]).rjust(9), str(split_lines[i][1]).rjust(9),\
-                str(split_lines[i][2]).rjust(9), str(split_lines[i][3]).rjust(9), str(split_lines[i][4]).rjust(9)
+            print str(person_list[i].name).rjust(9), str(person_list[i].is_blocked).rjust(9),\
+                str(person_list[i].is_limit_on).rjust(9), str(person_list[i].is_admin).rjust(9), \
+                str(person_list[i].password).rjust(9)
         except IndexError:
             continue
 
 
-def add_to_file(line):
-    file = open('data.txt', 'a')
-    file.write(line)
-    file.close()
+def add_to_file(append_line):
+    open_to_append = open('data.txt', 'a')
+    open_to_append.write(append_line)
+    open_to_append.close()
 
 
 def add_user():
-    split_lines = read_from_file()
-    new_user = User
+    new_user = User()
     while True:
         new_user.name = raw_input('Enter user"s name: ')
-        new_user.is_user_blocked = False
-        new_user.limit_on_password = False
-        new_user.is_admin = False
-        if new_user.name not in [split_lines[item][0] for item in range(len(split_lines))]:
-            new_user.password = ''
-            line = new_user.name + ' ' + str(new_user.is_user_blocked) + ' ' + str(new_user.limit_on_password) + \
+        if new_user.name not in [person_list[item].name for item in range(len(person_list))]:
+            line = new_user.name + ' ' + str(new_user.is_blocked) + ' ' + str(new_user.is_limit_on) + \
                    ' ' + str(new_user.is_admin) + ' ' + new_user.password + '\n'
             add_to_file(line)
             print 'User creates successfully'
@@ -164,15 +149,14 @@ def show_user_panel(login_name, password):
 
 
 def is_blocked():
-    split_lines = read_from_file()
     name = raw_input('Enter user name to block: ')
     name_in_file = False
-    for item in xrange(len(split_lines)):
-        if name == split_lines[item][0]:
+    for item in xrange(person_list.__len__()):
+        if name == person_list[item].name:
             name_in_file = True
-            if not ast.literal_eval(split_lines[item][1]):   # if user is not blocked
-                cur_line = str(split_lines[item][0]) + ' ' + str(split_lines[item][1])
-                new_line = str(split_lines[item][0]) + ' ' + str(not ast.literal_eval(split_lines[item][1]))
+            if not ast.literal_eval(person_list[item].is_blocked):   # if user is not blocked
+                cur_line = str(person_list[item].name) + ' ' + str(person_list[item].is_blocked)
+                new_line = str(person_list[item].name) + ' ' + str(not ast.literal_eval(person_list[item].is_blocked))
                 write_to_file(cur_line, new_line)
                 print 'User blocked successfully\n'
             else:
@@ -182,21 +166,20 @@ def is_blocked():
 
 
 def limit_on_password():
-    split_lines = read_from_file()
     name = raw_input('Enter user name to know if the limit on password: ')
-    for item in xrange(len(split_lines)):
-        if name == split_lines[item][0]:
-            print 'Current password is limited. ' + split_lines[item][2]
+    for item in xrange(person_list.__len__()):
+        if name == person_list[item].name:
+            print 'Current password is limited. ' + person_list[item].is_limit_on
             print 'Do you want to change user"s limit on password?'
             print '1. Yes\n2. No'
             choice = raw_input('Choice: ')
             if choice == '1':
-                cur_line = str(split_lines[item][0]) + ' ' + str(split_lines[item][1]) + ' ' + \
-                           str(split_lines[item][2]) + ' ' + str(split_lines[item][3]) + ' ' + \
-                           str(split_lines[item][4])
-                new_line = str(split_lines[item][0]) + ' ' + str(split_lines[item][1]) + ' ' + \
-                           str(not ast.literal_eval(split_lines[item][2])) + ' ' + \
-                           str(split_lines[item][3]) + ' ' + str(split_lines[item][4])
+                cur_line = str(person_list[item].name) + ' ' + str(person_list[item].is_blocked) + ' ' + \
+                           str(person_list[item].is_limit_on) + ' ' + str(split_lines[item].is_admin) + ' ' + \
+                           str(split_lines[item].password)
+                new_line = str(person_list[item].name) + ' ' + str(person_list[item].is_blocked) + ' ' + \
+                           str(not ast.literal_eval(person_list[item].is_limit_on)) + ' ' + \
+                           str(split_lines[item].is_admin) + ' ' + str(split_lines[item].password)
                 write_to_file(cur_line, new_line)
                 print 'You change user"s limit on password'
                 return
@@ -216,79 +199,65 @@ def show_admin_panel(login_name, password):
         print '\n'
         if choice == 1:
             print '\n-----Changing password--------'
-            change_password(login_name, password)                # don't works
+            change_password(login_name, password)
         elif choice == 2:
             print '\n-----List of users--------'
-            list_of_users()                                      # works
+            list_of_users()
         elif choice == 3:
             print '\n-------Adding new user-------'
-            add_user()                       # works
+            add_user()
         elif choice == 4:
             print '\n-------Blocking user-------'
-            is_blocked()                                         # works
+            is_blocked()
         elif choice == 5:
             print '\n-------Checking limit on password-------'
-            limit_on_password()                                  # works
+            limit_on_password()
         elif choice == 6:
-            show_menu()                                          # works
+            return
+            # show_menu()
         else:
             print 'Incorrect choice'
             continue
 
 
 def login():
-    split_lines = read_from_file()
     login_name = raw_input('Login: ')
     for i in xrange(3):
-        if login_name == split_lines[0][0] and split_lines[0][3] == 'True':   # check if admin
-            try:
+        if login_name == person_list[0].name and person_list[0].is_admin == 'True':   # check if admin
+            # try:
+            password = raw_input('Password: ')
+            if person_list[0].password == '' and len(password) == 0:
+                show_admin_panel(person_list[0].name, person_list[0].password)
+                return
+            elif password == person_list[0].password:
+                show_admin_panel(person_list[0].name, person_list[0].password)
+                return
+            else:
+                print 'Admin password is incorrect, try again'
+                continue
+        elif login_name in [person_list[item].name for item in xrange(person_list.__len__())]:
+            if not (ast.literal_eval(str([person_list[item].is_blocked for item in xrange(person_list.__len__())
+                                          if login_name == person_list[item].name][0]))):
                 password = raw_input('Password: ')
-                if len([split_lines[item] for item in xrange(len(split_lines)) if login_name ==
-                        split_lines[0][0]][0]) < 5 and len(password) == 0:
-                    show_admin_panel(login_name, password)
-
-                elif password == split_lines[0][4]:
-                    show_admin_panel(login_name, password)
+                if person_list[4].password == '' and len(password) == 0:
+                    show_user_panel(login_name, password)
+                    return
+                elif password == [person_list[item].password for item in range(person_list.__len__())
+                                  if login_name == person_list[item].name][0]:
+                    show_user_panel(login_name, password)
+                    return
                 else:
-                    print 'Admin password is incorrect, try again'
-                    continue
-            except IndexError:
-                print 'Incorrect password'
-                show_menu()
-            except ValueError:
-                print 'Incorrect password'
-                show_menu()
-
-        # user in file and not blocked
-        elif login_name in [split_lines[item][0] for item in xrange(len(split_lines))]:
-            if not (ast.literal_eval(str([split_lines[item][1] for item in xrange(len(split_lines))
-                                          if login_name == split_lines[item][0]][0]))):
-                try:
-                    password = raw_input('Password: ')
-
-                    if len([split_lines[item] for item in xrange(len(split_lines)) if login_name ==
-                            split_lines[item][0]][0]) < 5 and len(password) == 0:
-                        show_user_panel(login_name, password)
-                    elif password == [split_lines[item][4] for item in range(len(split_lines))
-                                      if login_name == split_lines[item][0]][0]:
-                        show_user_panel(login_name, password)
+                    if i == 2:
+                        print 'You entered wrong password tree times! \n'
+                        exit()
                     else:
-                        if i == 2:
-                            print 'You entered wrong password tree times! \n'
-                            exit()
-                        else:
-                            print 'User password is incorrect, try again'
-                            continue
-                except IndexError:
-                    print 'Incorrect password'
-                    show_menu()
-                except ValueError:
-                    print 'Incorrect password'
-                    show_menu()
+                        print 'User password is incorrect, try again'
+                        continue
             else:
                 if i == 2:
                     print 'You are blocked'
         else:
+            print 'Incorrect login'
             return
 
 
@@ -310,14 +279,28 @@ def show_menu():
         elif choice == '2':
             info()
         elif choice == '3':
-            exit()
+            return
         else:
             print 'Incorrect choice'
             continue
 
 
 if __name__ == '__main__':
-    file_name = 'file.txt'
+    file = open('data.txt', 'r')
+    split_lines = []
+    for line in file:
+        split_lines.append(line.replace('\n', '').split(' ', -1))
+    file.close()
+    
+    global person_list
+    person_list = []
+    for person_arr in split_lines:
+        if len(person_arr) < 5:
+            temp = User(person_arr[0], person_arr[1], person_arr[2], person_arr[3], '')
+        else:
+            temp = User(person_arr[0], person_arr[1], person_arr[2], person_arr[3], person_arr[4])
+        person_list.append(temp)
+
     if not os.path.exists(file_name):
         f = open(file_name, 'w')
         f.write('ADMIN False False True ')  # admin login, is_blocked, limit_on, is_admin
