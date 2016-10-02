9b081efe19a492e9f28c2b927e98b6f5c09bfb72
Marta Fedyshyn <martavfedyshyn@gmail.com>
2016-10-02 20:18:41 +0300

    Update user.py

--- lab_1/src/user.py
+++ lab_1/src/user.py
@@ -1,11 +1,8 @@
 class User:
 
-    def __init__(self):
-        self.name = ''
-        self.is_blocked = False
-        self.is_limit_on = False
-        self.is_admin = False
-        self.password = ''
-
-# user = User()
-# user.change_password('marta', 'mmm')
+    def __init__(self, name='', is_blocked=False, is_limit_on=False, is_admin=False, password=''):
+        self.name = name
+        self.is_blocked = is_blocked
+        self.is_limit_on = is_limit_on
+        self.is_admin = is_admin
+        self.password = password
